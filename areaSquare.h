77388619a70f4f8198d6d5f4395738b4597b363b
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-10-20 09:56:26 -0300

    Function areaSquare

---
+++ areaSquare.h
@@ -0,0 +1,12 @@
+//
+// Created by Caio Ozolins Mendes on 20/10/2022.
+//
+
+#ifndef EXERCICIOS_FUNCOES_E_MACROS_AREASQUARE_H
+#define EXERCICIOS_FUNCOES_E_MACROS_AREASQUARE_H
+
+#include <math.h>
+
+#define areaSquare(sideLen) (pow(sideLen, 2))
+
+#endif //EXERCICIOS_FUNCOES_E_MACROS_AREASQUARE_H
--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "areaCircle.h"
 #include "volumeSphere.h"
+#include "areaSquare.h"
 
 int main(){
 
-    float radius;
+    float radius, sideSquare;
 
     printf("Type the radius of the circle (cm):");
     scanf("%f", &radius);
@@ -12,6 +13,9 @@ int main(){
     printf("Type the radius of the sphere (cm):");
     scanf("%f", &radius);
     printf("The volume of the sphere is %.2fcm%c\n", volumeSphere(radius), 252);
+    printf("Type the length of the square (cm):");
+    scanf("%f", &sideSquare);
+    printf("The area of the circle is %.2fcm%c\n", areaSquare(sideSquare), 253);
 
     return 0;
 }
