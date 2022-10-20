2f36c4ae411f43089db6205ec3601475d88f9493
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-10-20 09:51:12 -0300

    Function volumeSphere

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "areaCircle.h"
+#include "volumeSphere.h"
 
 int main(){
 
     float radius;
 
-    printf("Type the radius (cm):");
+    printf("Type the radius of the circle (cm):");
     scanf("%f", &radius);
-    printf("The area of the circle is %.2fcm%c", areaCircle(radius), 253);
+    printf("The area of the circle is %.2fcm%c\n", areaCircle(radius), 253);
+    printf("Type the radius of the sphere (cm):");
+    scanf("%f", &radius);
+    printf("The volume of the sphere is %.2fcm%c\n", volumeSphere(radius), 252);
 
     return 0;
 }
---
+++ volumeSphere.h
@@ -0,0 +1,8 @@
+#ifndef EXERCICIOS_FUNCOES_E_MACROS_VOLUMESPHERE_H
+#define EXERCICIOS_FUNCOES_E_MACROS_VOLUMESPHERE_H
+
+#include <math.h>
+
+#define volumeSphere(radius) (((double)4/3)*M_PI*pow(radius, 3))
+
+#endif //EXERCICIOS_FUNCOES_E_MACROS_VOLUMESPHERE_H
